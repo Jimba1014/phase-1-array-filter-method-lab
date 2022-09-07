819caf8c415a9c5fa9a1e16d4b74980c1cfdf234
Jim <jimba.ed@outlook.com>
2022-09-07 12:40:21 -0400

    commit tree 2800f61aee0474f952d3dc2a4da1922eb9d717f3

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+function findMatching(names, personName){
+    const nameMatch = names.filter(function(item){
+        let lowerCaseitem = item.toLowerCase();
+        let personLowerCase = personName.toLowerCase();
+        return lowerCaseitem === personLowerCase
+    })
+    return nameMatch
+}
+
+
+
+
+function fuzzyMatch(nameArray, pattern){
+     let weirdCase = nameArray.filter(function(name){  
+       return name.startsWith(pattern)})
+    return weirdCase    
+}
+// function fuzzyMatch(nameArray, pattern){
+//    return nameArray.filter(function(name){  
+//         return name.startsWith(pattern)})
+//      }
+
+function matchName(driverArray,personName){
+    let personValue = driverArray.filter(function(user){
+        return user.name === personName
+    })
+    return personValue
+}
